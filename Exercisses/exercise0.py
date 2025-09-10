4682d683392fd7769551a11ef97572195a05af1c
Hamid Sherzad <Hamid.sherzad@iths.se>
2025-09-10 15:04:32 +0200

    Update

---
+++ Exercisses/exercise0.py
@@ -0,0 +1,40 @@
+#Uppgift: 1a
+import math
+
+a = 3
+b = 4
+
+c = math.sqrt(a**2 + b**2)
+
+print(c)
+
+#Uppgift: 1b
+
+import math
+
+c = 7.0
+a = 5.0
+
+b = math.sqrt(c**2 - a**2)
+
+print(b)
+
+# annorlunda "lösning"
+print(f"Länged av katetern b är{b: .1f} cm.")
+
+#uppgift: 2
+
+x = 300
+y = 365
+
+accuracy = 300/365
+
+print(accuracy)
+
+#alternativt
+procent = accuracy*100
+
+print(f"Träffsäkerheten av modellen är{procent:.2f}%.")
+
+#uppgift 3
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # python-programming-Hamid-Sherzad
+Programmering och annat gott längs äventyret, uppgifter och andra läxor.
