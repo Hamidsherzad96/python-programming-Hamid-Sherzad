b512340b306b54ab65d7611769084905784e22af
Hamid Sherzad <Hamid.sherzad@iths.se>
2025-09-10 15:51:20 +0200

    Exercise 0

--- Exercisses/exercise0.py
+++ Exercisses/exercise0.py
@@ -36,5 +36,53 @@ procent = accuracy*100
 
 print(f"Träffsäkerheten av modellen är{procent:.2f}%.")
 
-#uppgift 3
+#uppgift: 3
 
+tp = 2
+fp = 2
+fn = 11
+tn = 985
+
+accuracy = (tp+tn)/(tp+tn+fp+fn)
+
+print(accuracy)
+
+#alternativt:
+print(f"{accuracy: .2f}")
+
+#Uppgift: 4
+
+x1=4
+y1=4
+x2=0
+y2=1
+
+k=(y2-y1)/(x2-x1)
+m = y2-k*x2
+
+print(f"Formeln för linjen är y={k}x+{m:.0f}.\n")
+
+k = (4-1)/(4-0)
+
+print(k)
+
+#Uppgift: 5
+
+p1 = (3, 5)
+p2 = (-2, 4)
+
+distance = math.sqrt((p2[0]-p1[0])**2+(p2[1]-p1[1])**2)
+
+print(f"Längden är {distance:.1f}cm")
+
+#Uppgift: 6
+
+p1 = (2, 1, 4)
+p2 = (3, 1, 0)
+
+distance = math.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2 + (p1[2]-p2[2])**2)
+
+print(f"Längden mellan de två punkterna är ca {distance:.2f}")
+
+#Klappat och klart
+                     
---
+++ Untitled-1.py
@@ -0,0 +1,6 @@
+p1 = (3, 5)
+p2 = (-2, 4)
+
+distance = math.sqrt((p2[0]-p1[0])**2+(p2[1]-p1[1])**2)
+
+print(f"Längden är {distance:.1f}cm")
